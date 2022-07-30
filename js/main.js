8875779e048bd2b596a224b0d0de86b7297850d2
user.email <developerjenzii@gmail.com>
2022-07-30 23:16:51 +0200

    Take valid operand as input return either A or B.

--- js/main.js
+++ js/main.js
@@ -1,32 +1,50 @@
 var calculation = {};
-calculation.operandA = "3";
+calculation.operandA = undefined;
 calculation.operation = "*";
-calculation.operandB = "7";
+calculation.operandB = undefined;
+calculation.result = "";
 console.log(calculation);
 const operandButtons = document.querySelectorAll('.operand');
 const operatorButtons = document.querySelectorAll('.operator');
+const assignmentOperator = document.querySelector('.assignment');
 
-function getButtonValue() {
-
-  calculation.operandA = this.innerText;
+function getOperandValue() {
+let validNumber = /^[0-9]+$/;
+  if(validNumber.test(this.innerText) && (calculation.operandA == undefined)) {
+    calculation.operandA = this.innerText;
+    console.log(this.innerText);
+  } else if(validNumber.test(this.innerText)) {
+    calculation.operandB = this.innerText;
+    console.log(this.innerText);
+  }
   console.log(calculation);
 }
 
-operandButtons.forEach(button => button.addEventListener('click', getButtonValue));
+operandButtons.forEach(button => button.addEventListener('click', getOperandValue));
 
 function addNumbers() {
   console.log(calculation.operandA + calculation.operandB);
 }
 
-operatorButtons.forEach(button => button.addEventListener('click', computeNumbers));
+operatorButtons.forEach(button => button.addEventListener('click', getOperationValue));
 
 function computeNumbers(){
+  calculation.operation = this.innerText;
   switch (calculation.operation) {
-    case '*':
-      
+    case calculation.operation == 'x':
+      calculation.result = calculation.operandA * calculation.operandB;
       break;
-  
+      case '/':
+        calculation.result = calculation.operandA / calculation.operandB;
+      break;
+      case '+':
+        calculation.result = calculation.operandA + calculation.operandB;
+      break;
+      case '-':
+        calculation.result = calculation.operandA - calculation.operandB;
+      break;  
     default:
       break;
   }
+  console.log(calculation.result);
 }
